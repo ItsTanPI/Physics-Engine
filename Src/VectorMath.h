835b1c32cca1f25ae8a365ba712d10ee41708ba4
Shri Hari <115062948+ItsTanPI@users.noreply.github.com>
2024-04-10 21:42:41 +0530

    Add files via upload

--- Src/VectorMath.h
+++ Src/VectorMath.h
@@ -1,8 +1,9 @@
 #pragma once
 
 #include<iostream>
-#include<math.h>
+#include<cmath>
 
+#define PI 3.14159265f
 
 struct Vector2
 {
@@ -98,7 +99,7 @@ struct Vector2
 void Polygon(int sides, float Radius, float* Vertices, int* Indices)
 {
     float angle;
-    angle = (360.0f / sides) * 3.14159265f /180.0f;
+    angle = (360.0f / sides) * PI/180.0f;
     float IndexAngle = angle;
     
     int Index = (sides * 2) + 2;
